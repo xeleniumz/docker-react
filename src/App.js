41cfc517d065c252a5c88635b05c871f870f15e6
xeleniumz <fx@DESKTOP-SL32LOO.localdomain>
2021-10-15 07:59:08 +0700

    initialize project

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Bye there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          AAAA
         </a>
       </header>
     </div>
